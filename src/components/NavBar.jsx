4bc4473fb64e3148ab5966d135e1c8aede60b32e
Thabo <thvbojafta@gmail.com>
2024-06-07 17:13:37 +0200

    update Card and Hero components

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,7 @@
-export default function NavBar(){
-    return (
-        <nav>
-      
-    <img className="airbnb-logo" src="./images/airbnb-logo.png" alt="logo"  />
-  
-        </nav>
-    )
+export default function NavBar() {
+  return (
+    <nav>
+      <img className="airbnb-logo" src="./images/airbnb-logo.png" alt="logo" />
+    </nav>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import NavBar from './components/NavBar'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <NavBar />
-  </React.StrictMode>,
-)
+    <Hero />
+    <Card />
+  </React.StrictMode>
+);
